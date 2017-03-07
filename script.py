03198d7eea7570b8eec79c136495e874f92818d0
deadoggy <deadoggy@aliyun.com>
2017-03-07 11:25:26 +0800

    new file:   "201501to201612/\344\270\200\345\271\264\346\234\237\345\255\230\346\254\276\345\210\251\347\216\207(1).csv" 	new file:   "201501to201612/\344\270\200\345\271\264\346\234\237\345\255\230\346\254\276\345\210\251\347\216\207(2).csv" 	new file:   "201501to201612/\344\270\211\346\235\277\346\210\220\346\214\207.csv" 	new file:   "201501to201612/\344\270\211\346\235\277\346\210\220\346\214\207.xlsx" 	new file:   "201501to201612/\345\261\205\346\260\221\346\266\210\350\264\271\346\200\273\351\242\235.csv" 	new file:   result.txt 	modified:   script.py 	new file:   "\344\270\200\345\271\264\346\234\237\345\255\230\346\254\276\345\210\251\347\216\207.csv"

--- script.py
+++ script.py
@@ -11,27 +11,30 @@ sys.setdefaultencoding('utf8')
 
 
 class ProcessData:
+
+    __gap = 2
+
     def processCustomData(self):
         #数据格式：
         #时间	社会消费品零售总额_当期值(亿元)	社会消费品零售总额_累计值(亿元)
-        file = open(u'居民消费数据.csv')
+        file = open(u'居民消费总额.csv')
         record = []
         ret = []
 
-        line = file.readline().decode("gbk")
+        line = file.readline()
         while 0 != len(line):
             item = line.split(u',')
-            item[0] = item[0].replace(u'月', u'')
-            item[0] = item[0][item[0].index(u'年')+1: len(item[0])]
+            leng = len(item[0])
+            item[0] = item[0][6: len(item[0])-2]
 
-            if 0 == eval(item[0]) % 2:
+            if 0 == eval(item[0]) % self.__gap:
                 if 0 == len(ret)%6:
                     preval = 0
                 else:
                     preval = record[len(record)-1]
                 ret.append(eval(item[2]) - preval)
                 record.append(eval(item[2]))
-            line = file.readline().decode("gbk")
+            line = file.readline()
         index = len(ret)-1
         while index > 0:
             ret[index] = math.log(ret[index]/ret[index-1])
@@ -44,22 +47,31 @@ class ProcessData:
     def prodessStockData(self):
         #数据格式：
         #日期	股票代码	名称	收盘价
-        file = open(u'上证股指.csv')
+        file = open(u'三板成指.csv')
         ret = []
         record = []
 
         #读入数据
-        line = file.readline().decode('gbk')
+        line = file.readline().decode('gbk').encode('UTF-8')
         while 0!=len(line):
             line = line.split(',')
-            record.append([time.strptime(line[0], "%Y/%m/%d"), eval(line[3])])
+            record.append([time.strptime(line[0], "%Y-%m-%d"), eval(line[1])])
             line = file.readline().decode('gbk')
 
+
+        #以gap个月为单位的收益率
         monthEndFlag = []
         preFlag = []
+        gap = self.__gap  # 以几个月为单位，以后只要修改这里就可以了
         for dataItem in record:
 
-            if preFlag!= [] and 0 != dataItem[0].tm_mon%2 and dataItem[0].tm_mon != preFlag[0].tm_mon:
+            if 1 == gap:
+                gapCondition = True
+            else:
+                gapCondition = 0 != dataItem[0].tm_mon%gap
+
+
+            if preFlag!= [] and gapCondition and dataItem[0].tm_mon != preFlag[0].tm_mon:
                 if 0 != len(monthEndFlag):
                     ret.append(math.log(preFlag[1]/monthEndFlag[1]))
                 monthEndFlag = preFlag
@@ -70,17 +82,32 @@ class ProcessData:
         return ret
 
     def processInterestRate(self):
-        file = open(u'定期利率.csv')
+        file = open(u'一年期存款利率.csv')
+        temp = []
         ret = []
 
         line = file.readline()
         while 0 != len(line):
-            rate = eval(line)/600
-            for index in range(0,6):
-                ret.append(rate)
+            rate = eval(line) / 100
+            temp.append(rate)
             line = file.readline()
-        ret.pop(0)
+
+        for i in range(1, len(temp)):
+            ret.append(math.log(temp[i]/temp[i-1]))
         return ret
+
+
+
+
+        #以下为年度数据，需要除以月度
+        # line = file.readline()
+        # while 0 != len(line):
+        #     rate = eval(line)/1200
+        #     for index in range(0,6):
+        #         ret.append(rate)
+        #     line = file.readline()
+        # ret.pop(0)
+        # return ret
 
     def calc(self):
         stock = self.prodessStockData()
